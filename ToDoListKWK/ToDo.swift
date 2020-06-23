6b5ab07d4b22297e67ccae5955869f6229c09356
Shruti Patel <shruti.p2003@gmail.com>
2020-06-23 13:17:36 -0500

    ToDo List

---
+++ ToDoListKWK/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListKWK
+//
+//  Created by Shikha Patel on 6/22/20.
+//  Copyright © 2020 Shikha Patel. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    //initialize to empty string
+    var name = ""
+    //initialize boolean to false
+    var important = false
+    
+}
---
+++ ToDoListKWK/ToDoTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoListKWK
+//
+//  Created by Shikha Patel on 6/22/20.
+//  Copyright © 2020 Shikha Patel. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var toDos : [ToDo] = []
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+
+    // MARK: - Table view data source
+
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return toDos.count
+    }
+
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+      let toDo = toDos[indexPath.row]
+
+      if toDo.important {
+        cell.textLabel?.text = "★" + toDo.name
+      } else {
+        cell.textLabel?.text = toDo.name
+      }
+
+      return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      if let addVC = segue.destination as? AddToDoViewController {
+        addVC.previousVC = self
+      }
+
+      if let completeVC = segue.destination as? CompleteToDoViewController {
+        if let toDo = sender as? ToDo {
+          completeVC.selectedToDo = toDo
+          completeVC.previousVC = self
+        }
+      }
+    }
+}
